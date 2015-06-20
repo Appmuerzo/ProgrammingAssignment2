f641582c3ca50a694ca3c5ef9e95d1a530711211
Appmuerzo <Appmuerzo@users.noreply.github.com>
2015-06-20 02:03:16 -0500

    added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,33 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caching the Mean of a Vector
 
-## Write a short comment describing this function
+## 1st function: it creates a list that contains 4 member
 
 makeCacheMatrix <- function(x = matrix()) {
-    M <- NULL 
-    set <- function(b) {
-      x <<- b
+    M <- NULL # to store the inversion
+    set <- function(b) { # A setter function 
+      x <<- b 
       M <<- NULL 
     }
     
-    get <- function() x 
-    setI <- function(I) M <<- I 
-    getI <- function() M 
+    get <- function() x # return the input
+    setI <- function(I) M <<- I # set the inversed
+    getI <- function() M # return the inverted
     
-    list(set = set, get = get, setI = setI, getI = getI)
+    list(set = set, get = get, setI = setI, getI = getI) # return a list that contains functions
   }
 
 
-## Write a short comment describing this function
+## 2nd function: an assign, and if/else, set, and return
 
 cacheSolve <- function(x, ...) {
-  Mat <- x$getI()
-  if(!is.null(Mat)) { 
+  Mat <- x$getI() # get the inversed
+  if(!is.null(Mat)) { # if exists
     message("getting cached data")
-    return(Mat)
+    return(Mat) # return inversion
   }
-  data <- x$get() 
-  Mat <- solve(data) 
-  x$setI(Mat) 
-  Mat
+  data <- x$get() # else
+  Mat <- solve(data) # solve
+  x$setI(Mat) # set the inversed
+  Mat # return
 }
